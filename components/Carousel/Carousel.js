c8eabfdda7ebadefdeb7c25ebb260fc1ca80b9be
Kwang Woo Lee <kl467@cornell.edu>
2019-12-07 05:35:11 +0900

    Stretch complete - all bug fixed

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -80,30 +80,15 @@ const rightButton = document.querySelector('.right-button');
 leftButton.addEventListener('click', event => {
   let images = document.querySelectorAll('.img');  // images is an array-like object of images
   images.forEach(image => {
-    // updating the order of images in an infinite loop
-    if (image.classList.contains('toggle-off-1')) {
-      image.classList.remove('toggle-off-1');
-      image.classList.add('toggle-off-4');
-    } else if (image.classList.contains('toggle-on-left-2')){
-      image.classList.remove('toggle-on-left-2');
-      image.classList.add('toggle-off-1');
-    } else if (image.classList.contains('toggle-on-right-3')) {
-      image.classList.remove('toggle-on-right-3');
-      image.classList.add('toggle-on-left-2');
-    } else if (image.classList.contains('toggle-off-4')) {
-      image.classList.remove('toggle-off-4');
-      image.classList.add('toggle-on-right-3');
-    } 
-
-    // Change display
-    if (image.classList.contains('toggle-on-left-2')) {
-      image.style.display = 'block';
-    } else if (image.classList.contains('toggle-on-right-3')) {
-      image.style.display = 'block';
-    } else if (image.classList.contains('toggle-off-4')) {
-      image.style.display = 'none';
-    } else if (image.classList.contains('toggle-off-1')) {
-      image.style.display = 'none';
+    // change the source attribute
+    if (image.src === 'http://127.0.0.1:5502/assets/carousel/mountains.jpeg') {
+      image.src = 'http://127.0.0.1:5502/assets/carousel/computer.jpeg';
+    } else if (image.src === 'http://127.0.0.1:5502/assets/carousel/computer.jpeg') {
+      image.src = 'http://127.0.0.1:5502/assets/carousel/trees.jpeg';
+    } else if (image.src === 'http://127.0.0.1:5502/assets/carousel/trees.jpeg') {
+      image.src = 'http://127.0.0.1:5502/assets/carousel/turntable.jpeg';
+    } else if (image.src === 'http://127.0.0.1:5502/assets/carousel/turntable.jpeg') {
+      image.src = 'http://127.0.0.1:5502/assets/carousel/mountains.jpeg';
     }
   })
 })
@@ -111,33 +96,85 @@ leftButton.addEventListener('click', event => {
 rightButton.addEventListener('click', event => {
   let images = document.querySelectorAll('.img');  // images is an array-like object of images
   images.forEach(image => {
-    // updating the order of images in an infinite loop
-    if (image.classList.contains('toggle-off-4')) {
-      image.classList.remove('toggle-off-4');
-      image.classList.add('toggle-off-1');
-    } else if (image.classList.contains('toggle-off-1')){
-      image.classList.remove('toggle-off-1');
-      image.classList.add('toggle-on-left-2');
-    } else if (image.classList.contains('toggle-on-left-2')) {
-      image.classList.remove('toggle-on-left-2');
-      image.classList.add('toggle-on-right-3');
-    } else if (image.classList.contains('toggle-on-right-3')) {
-      image.classList.remove('toggle-on-right-3');
-      image.classList.add('toggle-off-4');
-    } 
-
-    // Change display
-    if (image.classList.contains('toggle-on-left-2')) {
-      image.style.display = 'block';
-    } else if (image.classList.contains('toggle-on-right-3')) {
-      image.style.display = 'block';
-    } else if (image.classList.contains('toggle-off-4')) {
-      image.style.display = 'none';
-    } else if (image.classList.contains('toggle-off-1')) {
-      image.style.display = 'none';
+    // change the source attribute
+    if (image.src === 'http://127.0.0.1:5502/assets/carousel/turntable.jpeg') {
+      image.src = 'http://127.0.0.1:5502/assets/carousel/trees.jpeg';
+    } else if (image.src === 'http://127.0.0.1:5502/assets/carousel/trees.jpeg') {
+      image.src = 'http://127.0.0.1:5502/assets/carousel/computer.jpeg';
+    } else if (image.src === 'http://127.0.0.1:5502/assets/carousel/computer.jpeg') {
+      image.src = 'http://127.0.0.1:5502/assets/carousel/mountains.jpeg';
+    } else if (image.src === 'http://127.0.0.1:5502/assets/carousel/mountains.jpeg') {
+      image.src = 'http://127.0.0.1:5502/assets/carousel/turntable.jpeg';
     }
   })
-  
-  leftImage = document.querySelector('.toggle-on-left');
-  console.log(images);
 })
+
+
+/*
+Below is the bug I created
+- If I shift the reading frame, the order of image messes up. 
+- Fix: If I shoft the content instead (not the reading frame), the order is still intact.
+*/
+
+// leftButton.addEventListener('click', event => {
+//   let images = document.querySelectorAll('.img');  // images is an array-like object of images
+//   images.forEach(image => {
+//     // updating the order of images in an infinite loop
+//     if (image.classList.contains('toggle-off-1')) {
+//       image.classList.remove('toggle-off-1');
+//       image.classList.add('toggle-off-4');
+//     } else if (image.classList.contains('toggle-on-left-2')){
+//       image.classList.remove('toggle-on-left-2');
+//       image.classList.add('toggle-off-1');
+//     } else if (image.classList.contains('toggle-on-right-3')) {
+//       image.classList.remove('toggle-on-right-3');
+//       image.classList.add('toggle-on-left-2');
+//     } else if (image.classList.contains('toggle-off-4')) {
+//       image.classList.remove('toggle-off-4');
+//       image.classList.add('toggle-on-right-3');
+//     } 
+
+//     // Change display
+//     if (image.classList.contains('toggle-on-left-2')) {
+//       image.style.display = 'block';
+//     } else if (image.classList.contains('toggle-on-right-3')) {
+//       image.style.display = 'block';
+//     } else if (image.classList.contains('toggle-off-4')) {
+//       image.style.display = 'none';
+//     } else if (image.classList.contains('toggle-off-1')) {
+//       image.style.display = 'none';
+//     }
+//   })
+// })
+
+// rightButton.addEventListener('click', event => {
+//   let images = document.querySelectorAll('.img');  // images is an array-like object of images
+//   let imagesArr = Array.from(images);
+//   imagesArr.forEach(image => {
+//     // updating the order of images in an infinite loop
+//     if (image.classList.contains('toggle-off-4')) {
+//       image.classList.remove('toggle-off-4');
+//       image.classList.add('toggle-off-1');
+//     } else if (image.classList.contains('toggle-off-1')){
+//       image.classList.remove('toggle-off-1');
+//       image.classList.add('toggle-on-left-2');
+//     } else if (image.classList.contains('toggle-on-left-2')) {
+//       image.classList.remove('toggle-on-left-2');
+//       image.classList.add('toggle-on-right-3');
+//     } else if (image.classList.contains('toggle-on-right-3')) {
+//       image.classList.remove('toggle-on-right-3');
+//       image.classList.add('toggle-off-4');
+//     } 
+
+//     // Change display
+//     if (image.classList.contains('toggle-on-left-2')) {
+//       image.style.display = 'block';
+//     } else if (image.classList.contains('toggle-on-right-3')) {
+//       image.style.display = 'block';
+//     } else if (image.classList.contains('toggle-off-4')) {
+//       image.style.display = 'none';
+//     } else if (image.classList.contains('toggle-off-1')) {
+//       image.style.display = 'none';
+//     }
+//   })
+// })
